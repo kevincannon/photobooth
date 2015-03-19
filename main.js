d4098cd24f7c2d2cc271989495a6e6f934d61541
kevincannon <kevin@multiblah.com>
2015-03-19 09:35:15 +0100

    Added viewer client. Functionally working, needs a lot of styling.

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var fs = require("fs")
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var fs = require("fs");
+var junk = require('junk');
+
+var path = require("path");
+
+// var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var ioClient = require('socket.io-client');
 var util = require('util');
 
 
 
+
+var SAVE_PATH = "views/";
 
 var SAVE_DIR = "saved_images/";
 
@@ -16,6 +26,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(express.static('views')); //assign static folder
+
+// app.use(express.static('saved_images')); //assign static folder
 
 
 // app.use(express.bodyParser());
@@ -34,7 +46,7 @@ app.post('/saveimage', function(request,response) {
 
 	// console.log(util.inspect(req, false, null));
   	var buffer = new Buffer(request.body.imgData, 'base64');
-  	fs.writeFile(SAVE_DIR + 'image_' + Date.now() + '.jpg', buffer);
+  	fs.writeFile(SAVE_PATH + SAVE_DIR + 'image_' + Date.now() + '.jpg', buffer);
 
   	// response.send("server received: " + request.body.imgData);
   	response.send("server received image!");
@@ -42,7 +54,7 @@ app.post('/saveimage', function(request,response) {
 });
 
 
-// Create server
+// Create web server
 var server = app.listen(3000, function () {
 
   var host = server.address().address
@@ -51,3 +63,36 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 });
+
+
+
+// Read Directory 
+function listAllImages() {
+  var fileArray;
+
+  fileArray = fs.readdirSync(SAVE_PATH + SAVE_DIR);
+  fileArray = fileArray.filter(junk.not);
+  
+  // Add web path to array
+  for (var i=0;i<fileArray.length;i++) {
+    fileArray[i] = "/" + SAVE_DIR + fileArray[i];  
+  }
+
+  //console.log(fileArray);
+  return(fileArray);
+}
+
+
+
+// Socket Stuff
+io.listen(server);
+
+
+io.on('connection', function(socket){
+  console.log('A client connected. Send current imag elist');
+
+  socket.emit('fullList', listAllImages().toString() );
+  // console.log( listAllImages().toString() );
+
+});
+
