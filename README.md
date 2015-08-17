8465e97db6814f61ca12333692b014667504fc58
kevincannon <kevin@multiblah.com>
2015-08-17 22:16:41 +0200

    Clean up - experimental

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Simple Photobooth
 
-**Status:** Works basically - Proceed with caution
+A very simple **Node.js** based photobooth, that can be run on a tablet for a party or similar event.
 
-**Goal** Create a very simple **Node.js** based photobooth, that can be run on a tablet for a party or similar event.
+It takes photos from the client's webcam, and then writes them to a directory on the server.
 
-It takes photos from the client's webcam, and then writes them to a directry on the server.
+A second client, displays a slideshow of the images which have been taken.
 
+**Status:** Personal project. It works, but it's very rough. Proceed with caution :)
 
 ##Running
 
-The server is responsible for runnign the client, and saving the images to the filesystem.
+The server is responsible for running the client, and saving the images to the filesystem.
 
 #####Downlaod & install node
 
@@ -22,21 +23,25 @@ https://nodejs.org/download/
 #####2. Install node modules
 
 	 npm install
-	 
+
 #####3. Run
 
 	 node main.js
-	 
+
 #####4. View
-	
-Then, open a browser and go to: **http://localhost:3000**
+
+Then, open a browser and go to: **http://localhost:3000 & http://localhost:3000/viewer/**
 
 
 -
+
+
+
+
+
 
 ####Making a thumbnail grid
 
 If you want to make a thumbnail grid afterwards, here's a useful ImageMagick command:
 
 	montage -geometry 360x480 -tile 13x8 -border 5  *.jpg montage.png
-
--- main.js
+++ main.js
@@ -1,44 +1,40 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var fs = require("fs");
-var junk = require('junk');
 
-var path = require("path");
 
-// var app = express();
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var ioClient = require('socket.io-client');
 var util = require('util');
 
+// File Stuff
+var fs = require("fs");
+var junk = require('junk');
+var path = require("path");
 
 
+
+
+// Key Variables
 
 var SAVE_PATH = "views/";
-
 var SAVE_DIR = "saved_images/";
 
+
+
+
+// Setup Server
 
 // Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.use(express.static('views')); //assign static folder
-
-// app.use(express.static('saved_images')); //assign static folder
+// assign static folder
+app.use(express.static('views'));
 
 
-// app.use(express.bodyParser());
-
-// app.get('/saveimage', function (req, res) {
-//   res.send('Hello World!')
-//   saveImage();
-// })
-
-
-// Receive post request
+// Receive post request - receive from camera page
 app.post('/saveimage', function(request,response) {
 
 	// console.log("node received: " + request.body.imgData);
@@ -70,16 +66,16 @@ var server = app.listen(3000, function () {
 
 
 
-// Read Directory 
+// Read Directory
 function listAllImages() {
   var fileArray;
 
   fileArray = fs.readdirSync(SAVE_PATH + SAVE_DIR);
   fileArray = fileArray.filter(junk.not);
-  
+
   // Add web path to array
   for (var i=0;i<fileArray.length;i++) {
-    fileArray[i] = "/" + SAVE_DIR + fileArray[i];  
+    fileArray[i] = "/" + SAVE_DIR + fileArray[i];
   }
 
   //console.log(fileArray);
@@ -99,4 +95,3 @@ io.on('connection', function(socket){
   // console.log( listAllImages().toString() );
 
 });
-
